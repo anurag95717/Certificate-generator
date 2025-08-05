fb20a7355c4b7dd981857146c2b4ba1f9719f076
Anurag Kumar Thakur <anuragkumarthakurthakur@gmail.com>
2025-08-05 10:54:58 +0530

    Add files via upload

---
+++ Certificate Generator/index.js
@@ -0,0 +1,69 @@
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase()
+  );
+
+submitBtn.addEventListener("click", () => {
+  const val = capitalize(userName.value);
+
+  
+  if (val.trim() !== "" && userName.checkValidity()) {
+   
+    generatePDF(val);
+  } else {
+    userName.reportValidity();
+  }
+});
+
+const generatePDF = async (name) => {
+  const existingPdfBytes = await fetch("./Cert.pdf").then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+
+  
+  const fontBytes = await fetch("./Sanchez-Regular.ttf").then((res) =>
+    res.arrayBuffer()
+  );
+
+ 
+  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+
+ 
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+
+  firstPage.drawText(name, {
+    x: 300,
+    y: 300,
+    size: 50,
+    font: SanChezFont,
+    color: rgb(0.2, 0.84, 0.67),
+  });
+
+
+  const pdfBytes = await pdfDoc.save();
+  console.log("Done creating");
+
+  
+
+  var file = new File(
+    [pdfBytes],
+    "Appreciation Certificate.pdf",
+    {
+      type: "application/pdf;charset=utf-8",
+    }
+  );
+ saveAs(file);
+};
+
